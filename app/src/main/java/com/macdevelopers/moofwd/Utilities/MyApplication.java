1be0ca3830d1401bbf4d34636d46593334374093
suraj <suraj@kesari.in>
2019-04-22 21:51:57 +0530

    finalPush

--- app/src/main/java/com/macdevelopers/moofwd/Utilities/MyApplication.java
+++ app/src/main/java/com/macdevelopers/moofwd/Utilities/MyApplication.java
@@ -24,7 +24,6 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
         mInstance = this;
         initFresco();
     }
--- app/src/main/java/com/macdevelopers/moofwd/Utilities/MyLog.java
+++ app/src/main/java/com/macdevelopers/moofwd/Utilities/MyLog.java
@@ -4,7 +4,7 @@ import android.util.Log;
 
 public class MyLog {
 
-    private static final boolean LOGGING = true; //false to disable logging
+    private static final boolean LOGGING = false; //false to disable logging
 
     public static void d(String tag, String message) {
         if (LOGGING) {
